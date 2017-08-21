7bcd1cb75fb6c53ddf78bd994dacbce2ddc5af21
root <yohgenius@gmail.com>
2017-08-21 15:01:44 +0700

    the project is not finished

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, AppRegistry, View } from 'react-native';
+
+const contacts = {[
+    {
+        id: 1,
+        firstName: "Amy",
+        lastName: "Taylor",
+        title: "CEO",
+        phone: "617-123-4567",
+        mobilePhone: "617-987-6543",
+        email: "amy@fakemail.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg"
+    },
+    {
+        id: 2,
+        firstName: "Anup",
+        lastName: "Gupta",
+        title: "VP of Engineering",
+        managerId: 1,
+        managerName: "Amy Taylor",
+        phone: "617-123-4567",
+        mobilePhone: "617-987-6543",
+        email: "anup@fakemail.com",
+        picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/anup_gupta.jpg"
+    }
+};
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello, Boss!</Text>
+        <Text>Hello, Boss!</Text>
+        <Text>Hello, Boss!</Text>
       </View>
     );
   }
---
+++ react-native-packager-cache-edd7e41768398dadc05751b4d24e500bceb72deb/cache/1e/bd2ae3391e07dbe4be40a10ee9ac357d6d5c05
@@ -0,0 +1,2 @@
+var core = module.exports = { version: '2.5.0' };
+if (typeof __e == 'number') __e = core;
